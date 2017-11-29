4c356e426d136e65dc370a0a6d39e82bc6803928
root <t.hoshman@gmail.com>
2017-11-29 08:46:22 -0800

    setting up web scraper

--- app/server/middleware/searchArticles.js
+++ app/server/middleware/searchArticles.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import moment from 'moment';
+import Article from '../database/models/article.js';
+import noodle from 'noodlejs';
 
 const searchArticles = (request, response, next) => {
 
@@ -10,7 +12,9 @@ const searchArticles = (request, response, next) => {
   let url = `https://newsapi.org/v2/everything/?from=${beginDate}&to=${endDate}&sortBy=${sortBy}&language=en&apiKey=${process.env.NEWS_KEY}`;
 
   if (topics) {
+    console.log('topics', topics)
     const formattedTopic = topics.join('%20OR%20').split(' ').join('%20');
+    console.log('formatted topics', formattedTopic)
     url += `&q=${formattedTopic}`;
   }
   if (selectedSources) {
@@ -30,7 +34,49 @@ const searchArticles = (request, response, next) => {
   axios
     .get(url)
     .then((newsResponse) => {
-      request.articles = newsResponse.data.articles;
+
+      let artsToSend = [];
+      newsResponse.data.articles.forEach((art) => {
+        Article.find({ url: art.url})
+        .then((res) => {
+          if (res.length) {
+            artsToSend.push(res);
+          } else {
+
+            let query = {
+              url: art.url,
+              selector: 'p',
+              extract: 'text',
+            }
+
+            noodle.query(query).then((results) => {
+              console.log('from noodle', results.results[0].results);
+              let vals = Object.assign({}, art)
+              vals.body =  results.results[0].results;
+              let artToSave = new Article(vals);
+              artsToSend.push(artToSave);
+              artToSave.save()
+              .then((saved) => {
+                console.log('Saved', saved);
+              })
+            })
+
+
+
+          }
+        })
+        
+      })
+      ///////////////////////////////
+      // here is where we need to  //
+      // check db                  //
+      // if it's not in the db {   //
+      // send the web crawler      //
+      //     store into the db     //
+      // and store locally  }      //
+      // else { store locally }    //
+      ///////////////////////////////
+      request.articles = artsToSend;
     })
     .catch((err) => {
       console.log('error newsAPI: ', err);
