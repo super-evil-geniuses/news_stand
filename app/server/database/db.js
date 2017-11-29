b838d44fb4216f3ec99a95189cec8d82f22d610a
Caleb Choi <calebc1205@gmail.com>
2017-11-28 19:56:44 -0800

    deleted some unnecessary code

--- app/server/database/db.js
+++ app/server/database/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/news_stand');
+mongoose.connect(process.env.MONGODB_URI);
 
 const db = mongoose.connection;
 
