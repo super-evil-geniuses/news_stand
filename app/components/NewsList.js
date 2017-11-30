d036924e46f7bc693319accda6e99ca26591d31f
alfonso <alfonso@hansacanada.com>
2017-11-30 00:35:43 -0800

    adding more to full article feature

--- app/components/NewsList.js
+++ app/components/NewsList.js
@@ -9,23 +9,6 @@ import NewsItem from './NewsItem';
 // Link is your replacement for anchor tags.
 // Route is the conditionally shown component based on matching a path to a URL.
 
-// var  <Switch>
-//     <Route exact path='/articles' 
-//       render={() =>  
-//       <div className="news-list">
-//         {props.newsArticles.length === 0 ?
-//           <div id="no-articles">No articles found. Please try another search</div> :
-//           props.newsArticles && uniq(props.newsArticles, i => i.url)
-//             .map(article => (
-//               <NewsItem article={article} key={article.url} />
-//           ))
-//         }
-//       </div>
-//     }
-//     />
-//     <Route path='/articles/:id' component={FullArticle}/>
-//   </Switch>
-
 const NewsList = props => (
   <div className="news-list">
     {props.newsArticles.length === 0 ?
