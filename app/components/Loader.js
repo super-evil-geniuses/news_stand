34daa5715aeba2b71af2fab2f5aec1c01427a54a
root <t.hoshman@gmail.com>
2017-12-02 10:14:24 -0800

    added loading image

---
+++ app/components/Loader.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Loader = (props) => (
+	<div>
+    <img src="https://i.imgur.com/seuaOqf.gif" alt="Loading" title="Loading" />
+	</div>)
+
+  export default Loader;
